7aeeabde04559a56a4db91891f92ed13dc0b74c7
larson <larson920@gmail.com>
2017-06-19 00:00:13 +0200

    premier commit du projet perso angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { EventService } from './event.service';
 
 import { AppComponent } from './app.component';
+import { HeaderAppComponent } from './header-app/header-app.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { ContaintContentComponent } from './containt-content/containt-content.component';
+import { CardComponent } from './card/card.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+
+const appRoutes: Routes = [
+  { path: 'events', component: CardComponent },
+  { path: 'event/:id', component: EventDetailComponent },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAppComponent,
+    JumbotronComponent,
+    ContaintContentComponent,
+    CardComponent,
+    EventDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
